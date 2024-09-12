4c504ba68d6af117654a2bdbd52e157c57ead676
turwokts <48319429+TEKKSUNN@users.noreply.github.com>
2024-09-12 22:13:23 +0800

    Add webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require("path");
+
+module.exports = {
+    mode: "production",
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};
